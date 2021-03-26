23da32a4f32c92aab7efbe20547801f4d96e0750
Akshay Nair <phenax5@gmail.com>
2021-03-26 17:45:53 +0530

    changes build system to webpack

---
+++ webpack.config.js
@@ -0,0 +1,68 @@
+const path = require('path');
+const glob = require('glob');
+const TerserPlugin = require('terser-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const PurgeCSSPlugin = require('purgecss-webpack-plugin');
+const { CleanWebpackPlugin: Cleanup } = require('clean-webpack-plugin');
+const HtmWebpackPlugin = require('html-webpack-plugin');
+
+const buildDir = path.resolve('./build');
+const srcDir = path.resolve('./src');
+
+const ENV = 'production';
+
+const production = ENV === 'production';
+
+module.exports = {
+  entry: path.join(srcDir, 'index.js'),
+  mode: ENV,
+  output: {
+    path: buildDir,
+    filename: '[name].[contenthash].js',
+    chunkFilename: '[name].[contenthash].js',
+  },
+  optimization: {
+    minimize: production,
+    moduleIds: 'natural',
+    minimizer: [
+      new TerserPlugin({
+        parallel: true,
+        terserOptions: {
+          mangle: true,
+          compress: {},
+        },
+      }),
+    ],
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/i,
+        use: ['babel-loader'],
+      },
+      {
+        test: /\.css$/i,
+        use: [
+          !production
+            ? 'style-loader'
+            : MiniCssExtractPlugin.loader,
+          'css-loader',
+          'postcss-loader',
+        ],
+      },
+    ],
+  },
+  plugins: [
+    new PurgeCSSPlugin({
+      paths: glob.sync(path.join(buildDir, './**/*.js'), { nodir: true }).concat([
+        path.join(srcDir, './index.html'),
+      ]),
+    }),
+    new HtmWebpackPlugin({ title: 'Loading... | Polykode' }),
+    new MiniCssExtractPlugin({
+      filename: '[name].[contenthash].css',
+      chunkFilename: '[name].[contenthash].css',
+    }),
+    new Cleanup(),
+  ],
+};
